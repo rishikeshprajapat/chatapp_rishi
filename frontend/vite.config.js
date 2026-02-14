f6a781258c0cf4fd1e6a3acc784a7854fa75d341
rishikeshprajapat <rishikeshprajapat6376@gmial.com>
2026-02-14 21:44:22 +0530

    refactor: update Vite configuration to load environment variables dynamically

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-const backendUrl = import.meta.env.VITE_API_URL
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-		port: 8000,
-		proxy: {
-			"/api": {
-				target: backendUrl,
-			},
-		},
-	},
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const backendUrl = env.VITE_API_URL
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: backendUrl,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
