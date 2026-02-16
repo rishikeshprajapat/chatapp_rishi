1e2ca10ec9ed388d6c5762e72b672a8ee89a4a94
rishikeshprajapat <rishikeshprajapat6376@gmial.com>
2026-02-16 22:24:41 +0530

    refactor: set sameSite attribute to 'none' in cookie settings

--- backend/utils/genetareToke.js
+++ backend/utils/genetareToke.js
@@ -7,6 +7,7 @@ const generateTokenAndSetCookie=(userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:15*24*60*60*1000,
         httpOnly:true,
+        sameSite:"none",
         secure:process.env.NODE_ENV!=="development"
     });
 };
