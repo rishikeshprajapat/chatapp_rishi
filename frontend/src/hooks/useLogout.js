1d1ebb47565666646080cf532c8cdc36e2e0ae67
rishikeshprajapat <rishikeshprajapat6376@gmial.com>
2026-02-16 22:30:19 +0530

    refactor: add credentials option to fetch requests in authentication hooks

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -13,6 +13,7 @@ const useLogout = () => {
 			const res = await fetch(`${backendUrl}/api/auth/logout`, {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
+				credentials:"include"
 			});
 			const data = await res.json();
 			if (data.error) {
